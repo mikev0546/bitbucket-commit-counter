915fc52a295674cc9d00b837a1936c9514b7741f
mikev <mikevvossen@hotmail.com>
2022-12-07 22:13:06 +0100

    Init

---
+++ counter.py
@@ -0,0 +1,82 @@
+import requests, dateutil.parser
+import base64
+import math
+
+
+# Configuration
+year = 2020
+login_string = "YOUR_BITBUCKET_USERNAME:YOUR_BITBUCKET_APP_KEY"
+workspace = "stijlgenoten"
+pagelen = 100
+
+#vars
+totalCommits = 0
+commitCount = 0
+commits = []
+
+print ("")
+print ("Stats for {year}".format(year=year))
+print ("")
+
+
+login_string_bytes = login_string.encode("ascii")
+base64_login_bytes = base64.b64encode(login_string_bytes)
+base64_string = base64_login_bytes.decode("ascii")
+r = requests.get('https://api.bitbucket.org/2.0/repositories/{workspace}/?q=updated_on>="{year}-01-01"&pagelen={pagelen}'.format(year=year, workspace=workspace, pagelen=pagelen), headers={'Authorization': 'Basic {base64_string}'.format(base64_string=base64_string)})
+
+counterPages = 0
+commitsAuthors = {}
+repoCommits = {}
+repos = r.json()
+amountOfPages = math.ceil(repos['size'] / pagelen);
+totalSize = repos['size']
+
+while counterPages < amountOfPages:
+	counterPages += 1
+	for repo in repos["values"]:
+		commitLink = repo["links"]["commits"]["href"] + '?pagelen={pagelen}'.format(pagelen=pagelen)
+		repoSlug = repo["slug"]
+		
+		# print(repoSlug)
+		# continue
+		r = requests.get(commitLink,
+		headers={'Authorization': 'Basic {base64_string}'.format(base64_string=base64_string)})
+		c = r.json()
+		commits.extend(c['values'])
+		while 'next' in c:
+			# print("next page")
+			r = requests.get("{next}".format(next=c['next']), 
+				headers={'Authorization': 'Basic {base64_string}'.format(base64_string=base64_string)})
+			c = r.json()
+			commits.extend(c['values'])
+
+		for commit in commits:
+			#print("count commit")
+			commitDate = dateutil.parser.parse(commit['date'])
+			if commitDate.year == year:
+				commitCount += 1
+				if commit['author']['raw'] in commitsAuthors:
+    					commitsAuthors[commit['author']['raw']] += 1
+				else:
+    					commitsAuthors[commit['author']['raw']] = 1
+				
+
+		if(commitCount >= 1):
+			repoCommits['stijlgenoten/{repo}'.format(repo=repoSlug)] = commitCount		
+			print ("Total authors: {authors}".format(authors=commitsAuthors))
+			print ("Total commits in stijlgenoten/{repo}: {count}".format(repo=repoSlug, count=commitCount))	
+		totalCommits += commitCount	
+		#reset counters
+		commitCount = 0
+		commits = []
+		
+	if "next" in repos and repos['next']is not None:
+		r = requests.get("{next}".format(next=repos['next']), headers={'Authorization': 'Basic {base64_string}'.format(base64_string=base64_string)})
+		repos = r.json()
+
+
+print("")
+print ("Total overall commits: {count}".format(count=totalCommits))
+print ("Total authors: {authors}".format(authors=commitsAuthors))
+print ("Total repoCommits: {authors}".format(authors=repoCommits))
+print("")
